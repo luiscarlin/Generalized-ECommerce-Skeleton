996299b6d091d0fe12da2a203b5a88822401957a
drewburns998 <andrew.t.burns@outlook.com>
2016-11-22 08:56:13 -0500

    release to prod

--- src/main/java/com/devopsbuddy/backend/service/MockEmailService.java
+++ src/main/java/com/devopsbuddy/backend/service/MockEmailService.java
@@ -11,14 +11,12 @@ public class MockEmailService extends AbstractEmailService {
 
     /** The Application Logger */
     private static final Logger LOG = LoggerFactory.getLogger(MockEmailService.class);
-    
-    
+
+
     @Override
     public void sendGenericEmailMessage(SimpleMailMessage message) {
-        LOG.debug("Simulating an email service .. ");
+        LOG.debug("Simulating an email service...");
         LOG.info(message.toString());
-        LOG.debug("Email sent");
-
-
+        LOG.debug("Email sent.");
     }
 }
