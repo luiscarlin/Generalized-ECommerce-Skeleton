7bc0ca4aecb7ca8c2656d4bffaf5688a37237160
drewburns998 <andrew.t.burns@outlook.com>
2016-11-22 21:09:19 -0500

    added mysql support

--- src/main/java/com/devopsbuddy/backend/service/UserService.java
+++ src/main/java/com/devopsbuddy/backend/service/UserService.java
@@ -49,26 +49,35 @@ public class UserService {
     @Transactional
     public User createUser(User user, PlansEnum plansEnum, Set<UserRole> userRoles){
 
-        String encryptedPassword = passwordEncoder.encode(user.getPassword());
-        user.setPassword(encryptedPassword);
+        User localUser = userRepository.findByEmail(user.getEmail());
 
-        Plan plan = new Plan(plansEnum);
-        // It makes sure the plans exist in the database
-        if (!planRepository.exists(plansEnum.getId())) {
-            plan = planRepository.save(plan);
+        if (localUser != null) {
+            LOG.info("User with username {} and email {} already exist. Nothing will be done. ",
+                    user.getUsername(), user.getEmail());
+        } else {
+
+            String encryptedPassword = passwordEncoder.encode(user.getPassword());
+            user.setPassword(encryptedPassword);
+
+            Plan plan = new Plan(plansEnum);
+            // It makes sure the plans exist in the database
+            if (!planRepository.exists(plansEnum.getId())) {
+                plan = planRepository.save(plan);
+            }
+
+            user.setPlan(plan);
+
+            for (UserRole ur : userRoles) {
+                roleRepository.save(ur.getRole());
+            }
+
+            user.getUserRoles().addAll(userRoles);
+
+            localUser = userRepository.save(user);
+
         }
 
-        user.setPlan(plan);
-
-        for (UserRole ur : userRoles) {
-            roleRepository.save(ur.getRole());
-        }
-
-        user.getUserRoles().addAll(userRoles);
-
-        user = userRepository.save(user);
-
-        return user;
+        return localUser;
     }
 
     /**
@@ -94,6 +103,11 @@ public class UserService {
         password = passwordEncoder.encode(password);
         userRepository.updateUserPassword(userId, password);
         LOG.debug("Password updated successfully for user id {} ", userId);
+
+        Set<PasswordResetToken> resetTokens = passwordResetTokenRepository.findAllByUserId(userId);
+        if (!resetTokens.isEmpty()) {
+            passwordResetTokenRepository.delete(resetTokens);
+        }
     }
 
 
